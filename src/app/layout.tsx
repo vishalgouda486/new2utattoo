f025639c1209076027ef2073507327c9c2509367
vishalgouda486 <vishalgouda486@gmail.com>
2026-04-10 10:42:40 +0530

    Added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Syncopate } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -130,7 +131,25 @@ export default function RootLayout({
 
   return (
     <html lang="en" data-scroll-behavior="smooth">
+
+      {/* ✅ META PIXEL ADDED HERE */}
+      <Script strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '763946492195884');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
       <body className={`${inter.variable} ${syncopate.variable} antialiased bg-black text-white`}>
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
